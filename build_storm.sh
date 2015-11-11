2fa544cda509bb7d72fa82afe2ce1b3e71ad56f6
John Bush <john.bush@traxtech.com>
2015-11-11 07:08:35 +0000

    make workie

--- build_storm.sh
+++ build_storm.sh
@@ -6,12 +6,12 @@ version=0.10.0
 description="Storm is a distributed realtime computation system. Similar to how Hadoop provides a set of general primitives
 for doing batch processing, Storm provides a set of general primitives for doing realtime computation. Storm is simple, can
 be used with any programming language, is used by many companies, and is a lot of fun to use!"
-url="http://storm-project.net"
+url="http://storm.apache.org/"
 arch="all"
 section="misc"
 package_version="~2"
-src_package="storm-${version}.zip"
-download_url="http://mirror.symnds.com/software/Apache/storm/apache-storm-0.10.0/apache-storm-0.10.0.tar.gz"
+src_package="apache-storm-${version}.zip"
+download_url="http://apache.mirrors.lucidnetworks.net/storm/apache-storm-0.10.0/apache-storm-0.10.0.zip"
 origdir="$(pwd)"
 storm_root_dir=/usr/lib/storm
 #use old debian init.d scripts or ubuntu upstart
@@ -43,20 +43,20 @@ fi
 mkdir -p build/var/log/storm
 mkdir -p build/var/lib/storm
 
-unzip ${origdir}/storm-${version}.zip
-rm -rf storm-${version}/logs
-rm -rf storm-${version}/log4j
-rm -rf storm-${version}/conf
-cp -R storm-${version}/* build${storm_root_dir}
+unzip ${origdir}/${src_package}
+rm -rf apache-storm-${version}/logs
+rm -rf apache-storm-${version}/log4j
+rm -rf apache-storm-${version}/conf
+cp -R apache-storm-${version}/* build${storm_root_dir}
 
 cd build
-cp ${origdir}/storm ${origdir}/storm-nimbus ${origdir}/storm-supervisor ${origdir}/storm-ui ${origdir}/storm-drpc etc/default
+cp ${origdir}/storm ${origdir}/storm-logviewer ${origdir}/storm-nimbus ${origdir}/storm-supervisor ${origdir}/storm-ui ${origdir}/storm-drpc etc/default
 cp ${origdir}/storm.yaml etc/storm
 cp ${origdir}/storm.log.properties etc/storm
 if [ $dist == "debian" ]; then
-  cp ${origdir}/init.d/storm-nimbus ${origdir}/init.d/storm-supervisor ${origdir}/init.d/storm-ui ${origdir}/init.d/storm-drpc etc/init.d
+  cp ${origdir}/init.d/storm-nimbus ${origdir}/init.d/storm-logviewer ${origdir}/init.d/storm-supervisor ${origdir}/init.d/storm-ui ${origdir}/init.d/storm-drpc etc/init.d
 else
-  cp ${origdir}/storm-nimbus.conf ${origdir}/storm-supervisor.conf ${origdir}/storm-ui.conf ${origdir}/storm-drpc.conf etc/init
+  cp ${origdir}/storm-nimbus.conf ${origdir}/storm-logviewer.conf ${origdir}/storm-supervisor.conf ${origdir}/storm-ui.conf ${origdir}/storm-drpc.conf etc/init
 fi 
 
 
