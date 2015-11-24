b5137a4cf1dc6aaddc2d5285a524ba73e6b6b9be
Mike Palmer <mike@mikepalmer.net>
2015-11-24 13:51:56 -0700

    Fixing postinst

--- after_install.sh
+++ after_install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/bin/chown -R storm:storm /var/log/storm /var/lib/storm
+/bin/chown -R storm:storm /var/log/storm /usr/lib/storm
 if [ ! -f /usr/local/bin/storm ]; then
   ln -s /usr/lib/storm/bin/storm /usr/local/bin/storm
 fi
