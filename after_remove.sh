8921c25f8e4eab757a3b7a6145db991b7d79e323
Mike Palmer <mike@mikepalmer.net>
2015-12-29 15:52:22 -0700

    Adding clarity to debian scripts

--- after_remove.sh
+++ after_remove.sh
@@ -1,3 +1,42 @@
 #!/bin/sh
-/usr/sbin/deluser storm
+# postrm script for storm
+
+set -e
+
+# summary of how this script can be called:
+#        * <postrm> `remove'
+#        * <postrm> `purge'
+#        * <old-postrm> `upgrade' <new-version>
+#        * <new-postrm> `failed-upgrade' <old-version>
+#        * <new-postrm> `abort-install'
+#        * <new-postrm> `abort-install' <old-version>
+#        * <new-postrm> `abort-upgrade' <old-version>
+#        * <disappearer's-postrm> `disappear' <overwriter>
+#          <overwriter-version>
+# for details, see http://www.debian.org/doc/debian-policy/ or
+# the debian-policy package
+
+
+case "$1" in
+    remove|upgrade|failed-upgrade|abort-install|abort-upgrade|disappear)
+    ;;
+
+    purge)
+        if getent passwd storm > /dev/null; then
+            deluser storm --quiet
+        fi
+        if getent group storm > /dev/null; then
+            delgroup storm --quiet
+        fi
+    ;;
+
+    *)
+        echo "postrm called with unknown argument \`$1'" >&2
+        exit 1
+    ;;
+esac
+
+#DEBHELPER#
+
+exit 0
 
